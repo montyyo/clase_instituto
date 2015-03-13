ca6551219425017a912875e5be831ffc146e0716
montyyo <marcoiban@gmail.com>
2015-03-13 10:20:04 +0100

    inicial metodo para añadir notas

--- Alumno.java
+++ Alumno.java
@@ -1,33 +1,66 @@
-
+import java.util.Random;
+import java.util.ArrayList;
 /**
- * Write a description of class Alumno here.
+ * clase para 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (marco) 
+ * @version (1-13/03/2015)
  */
 public class Alumno
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+  // instance variables - replace the example below with your own
+    private int nota[];
+    private int edad;
+    private int aula;
+    private String nombre;
+    public int media;
+    public int numero_asignaturas;
+    //creamos una constante final , no podemos cambiar el valor de esa constante
+   // private final int NOTA_MINIMA;
+    public  static final int NOTA_MINIMA=5;//variable de clase que almacena cnstante
     /**
      * Constructor for objects of class Alumno
      */
-    public Alumno()
+    public Alumno(String nombre,int edad)
     {
-        // initialise instance variables
-        x = 0;
+        this.nombre=nombre;
+        this.edad=edad;
+        this.nota=new int[0];
     }
-
+ 
+    public void setAula(int aula)
+    {
+        if(aula < 0)
+        {
+            this.aula=1;
+        }
+        else
+        {
+            this.aula=aula;
+        }
+    }
+    
+   
+    
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * cuantas notas introducimos
      */
-    public int sampleMethod(int y)
+    public void addNota(int nota)
     {
-        // put your code here
-        return x + y;
+        
+        int[] newnotas=new int[this.nota.length +1];
+        
+        if(this.nota.length == 0)
+        {
+            newnotas[(this.nota.length) -1] = nota;
+        }
+        else
+        {
+            newnotas[this.nota.length] = nota;
+        }
+       
+        this.nota=newnotas;
     }
+    
+    
 }
